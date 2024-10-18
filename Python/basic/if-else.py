5100283c276ae3047a2ea764baa6ebfde1867a3e
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-10-18 11:17:14 +0700

    Update if-else.py

--- Python/basic/if-else.py
+++ Python/basic/if-else.py
@@ -6,3 +6,18 @@ elif a == b:
     print("a dan b sama dengan")
 else:
     print("a lebih besar dari b")
+
+
+c = 20
+d = 200
+print("A") if c > d else print("B")
+
+e = 1000
+f = 1000
+print("A") if e > f else print("=") if e == f else print("B")
+
+g = 150
+h = 10
+i = 1000
+if g > h and i > g:
+    print("keduanya adalah benar")
