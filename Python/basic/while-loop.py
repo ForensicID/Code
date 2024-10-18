1c43e37d06399d9a0744f69ab167aaa92f2946aa
Randi Dwi Nur C <99389910+ForensicID@users.noreply.github.com>
2024-10-18 17:17:14 +0700

    Create while-loop.py

---
+++ Python/basic/while-loop.py
@@ -0,0 +1,20 @@
+i = 1
+while i < 8:
+    print(i)
+    if i == 4:
+        break
+    i += 1
+
+a = 0
+while a < 6:
+    a += 1
+    if a == 3:
+        continue
+    print(a)
+
+b = 1
+while b < 6:
+  print(b)
+  b += 1
+else:
+  print("b is no longer less than 6")
